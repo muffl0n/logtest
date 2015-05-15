b072f2b892fe719e755fd83ee002f27839f74c31
Sven Schliesing <sven@schliesing.de>
2015-05-15 13:26:04 +0200

    use Path.iterdir()

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ import logging
 import logging.config
 from pathlib import Path
 
+
 def main():
     logging.config.fileConfig('logging.conf')
     logger = logging.getLogger(__name__)
     logger = logging.LoggerAdapter(logger, {})
 
     path = Path('/tmp')
-    entries = list(path.glob('*'))
 
-    for entry in entries:
+    for entry in path.iterdir():
         logger.extra = {'entry': entry.name}
         logger.info(entry.stat().st_size)
 
